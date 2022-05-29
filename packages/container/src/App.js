1459d1347d68c71e845691ed0cd7e5de229a2e9a
webmatters <andrew.smith@webmatters.io>
2022-05-29 11:43:51 -0400

    Initial commit

---
+++ packages/container/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import MarketingApp from './components/MarketingApp'
+
+export default () => {
+  return (
+    <div>
+      <h1>Hi there from the Container!</h1>
+      <hr />
+      <MarketingApp />
+    </div>
+  )
+}
