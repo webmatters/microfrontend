9665b783262001746f874b240459249a1f361420
webmatters <andrew.smith@webmatters.io>
2022-06-01 10:32:07 -0400

    Adds dashboard sub app.

--- packages/container/src/components/Header.js
+++ packages/container/src/components/Header.js
@@ -81,6 +81,17 @@ export default function Header({ isSignedIn, onSignOut }) {
           >
             App
           </Typography>
+          {isSignedIn && (
+            <Typography
+              variant="h6"
+              color="inherit"
+              noWrap
+              component={RouterLink}
+              to="/dashboard"
+            >
+              Dashboard
+            </Typography>
+          )}
           <Button
             color="primary"
             variant="outlined"
